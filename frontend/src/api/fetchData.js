03030b35f4d48e3469f8f734f563cc944371ebae
Han-Seung-Chan <seungchanhan00@gmail.com>
2024-09-04 20:06:41 +0900

    feat : api 연동 준비

---
+++ frontend/src/api/fetchData.js
@@ -0,0 +1,27 @@
+const requestGET = async (url) => {
+  const res = await fetch(url);
+
+  if (res.ok) {
+    const json = await res.json();
+    return json;
+  }
+
+  throw new Error('요청 실패');
+};
+
+const requestPOST = async (url, body) => {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      ...body,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => console.log(result));
+};
+
+export const fetchContents = async (target, method, body = {}) => {
+  if (method === 'GET') await requestGET(`http://localhost:5001/${target}`);
+  else if (method === 'POST')
+    await requestPOST(`http://localhost:5001/${target}`, body);
+};
