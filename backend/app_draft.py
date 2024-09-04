0c247e428fc4d5d3eaf755f3c8e609c845f5fd56
ahram2 <helloahram1@gmail.com>
2024-09-04 17:13:01 +0900

    feat: list 기능 구현

--- backend/app_draft.py
+++ backend/app_draft.py
@@ -12,6 +12,24 @@ import uuid
 import datetime
 from bson.json_util import dumps
 
+from flask.json.provider import JSONProvider
+import json
+import sys
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+ 
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+app.json = CustomJSONProvider(app)
+
 @app.route('/write')
 def home():
     # return render_template('write.html')
@@ -52,31 +70,6 @@ def post_schedule():
     db.schedules.insert_one(doc)
     return jsonify({'result': 'success', 'post_id': post_id})
 
-# view 는 아직 못 함 
-@app.route('/view', methods=['GET'])
-# Get view 에서는 게시글의 상세 정보 + 댓글 출력
-# GET 으로 post_id 를 받아오기, 
-# DB 에서 post_id 를 찾아보기, 없으면 Error MSG 출력 
-def view_detail():
-    post_id = request.args.get('post_id')
-    if not post_id:
-        return jsonify({"result": "error", "message":" post_id 없음 "}), 400
-    
-    post = db.schedules.find_one({'post_id': post_id})
-    if not post:
-        return jsonify({"message": " post_id 못 찾음 "}), 404
-
-    return jsonify({ 
-            'result': 'success',
-            'post_id': post['post_id'],
-            'title': post['title'],
-            'scheduled_time': post['scheduled_time'],
-            'description': post['description'],
-            'write_time': post['write_time'],
-            'participant': post['participant'],
-            'author_id': post['author_id'],
-            # 'comments': comments
-        })
 
 @app.route('/list')
 def test():
@@ -88,21 +81,47 @@ def test():
 
     template = Template(template_content)
     rendered_template = template.render(variable1="value1", variable2="value2")
-    
-    # jinja2 연습
-    return render_template_string(template_content, title='Hello', variable1='jinja1', variable2='jinja2')
 
-# 이건 안 됨
-@app.route('/list', methods=['GET'])
+    return rendered_template
+
+@app.route('/api/list', methods=['GET'])
 def schedule_list():
     try:
-        schedules = list(db.schedules.find({}, {'_id': 0})) # id 제외하고 반환
-        for schedule in schedules:
-            print(schedule)  # 데이터 형태 확인
-        if not schedules:
-            return jsonify({'result': 'success', 'show_list': []})
-        return dumps({'result': 'success', 'show_list': schedules})
+        result = list(db.schedules.find({}, {'_id': 0})) # id 제외하고 반환
+        schedules = []
+        for schedule in result:
+            schedules.append(schedule)
+        print(schedules)
+        return jsonify({'result': 'success', 'schedules': schedules})
+
     except Exception as e:
         return jsonify({'result': 'error', 'message': str(e)}), 500
     
+ 
+# @app.route('/view', methods=['GET'])
+# # Get view 에서는 게시글의 상세 정보 + 댓글 출력
+# # GET 으로 post_id 를 받아오기, 
+# # DB 에서 post_id 를 찾아보기, 없으면 Error MSG 출력 
+# def view_detail():
+#     post_id = request.args.get('post_id')
+#     if not post_id:
+#         return jsonify({"result": "error", "message":" 해당 약속이 없습니다 "}), 400
+    
+#     post = db.schedules.find_one({'post_id': post_id})
+#     if not post:
+#         return jsonify({"message": " 해당 약속을 찾을 수 없습니다 "}), 404
+
+#     return jsonify({ 
+#             'result': 'success',
+#             'post_id': post['post_id'],
+#             'title': post['title'],
+#             'scheduled_time': post['scheduled_time'],
+#             'description': post['description'],
+#             'write_time': post['write_time'],
+#             'participant': post['participant'],
+#             'author_id': post['author_id'],
+#             # 'comments': comments
+#         })
+
+
 app.run(port=5001, debug=True)
